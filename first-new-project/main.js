26c261ba2963bb695a832e64c0aeec96d622c8d3
dlelele <108859281+dlelele@users.noreply.github.com>
2022-07-27 21:45:31 +0900

    Delete first-new-project directory

--- first-new-project/main.js
+++
@@ -1,6 +0,0 @@
-const togle=document.querySelector('.togle');
-const menu=document.querySelector('.header_menu');
-
-togle.addEventListener('click', () => {
-    menu.classList.toggle("active");
-})
